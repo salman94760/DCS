408c33f94ece2dbe3ca029f2c6c5d79142b30aba
salman94760 <salmankhan8501@gmail.com>
2026-09-19 02:24:56 +0530

    text change

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          Counts is {count}
         </button>
       </section>
 
