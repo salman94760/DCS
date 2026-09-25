8e6c654282ae9731630b77b81d189f4d839c9716
salman94760 <salmankhan8501@gmail.com>
2026-09-25 23:05:33 +0530

    remove side bar options

--- src/components/admin/Header.jsx
+++ src/components/admin/Header.jsx
@@ -21,8 +21,8 @@ export default function Header() {
         </div>
 
         <div className="relative z-10 flex items-center gap-6">
-          {/*<button className="relative text-slate-300 hover:text-white">*/}
-          {/* <svg
+          <button className="relative text-slate-300 hover:text-white">
+          <svg
               className="w-5 h-5"
               fill="none"
               stroke="currentColor"
@@ -34,12 +34,12 @@ export default function Header() {
                 strokeWidth="2"
                 d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
               />
-            </svg>*/}
-          {/*<span className="absolute -top-1 -right-1 bg-red-500 text-white text-[10px] w-4 h-4 rounded-full flex items-center justify-center">*/}
-          {/*2*/}
-          {/*   </span>
-          </button>*/}
-          {/*       <div className="flex items-center gap-2 text-white">
+            </svg>
+          <span className="absolute -top-1 -right-1 bg-red-500 text-white text-[10px] w-4 h-4 rounded-full flex items-center justify-center">*/}
+          2
+             </span>
+          </button>
+                <div className="flex items-center gap-2 text-white">
             <div className="w-9 h-9 rounded-full bg-blue-500 flex items-center justify-center font-semibold">
               A
             </div>
@@ -60,7 +60,7 @@ export default function Header() {
                 d="M19 9l-7 7-7-7"
               />
             </svg>
-          </div>*/}
+          </div>
         </div>
       </header>
     </>
